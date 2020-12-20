580da093cc4377e584d3b534a52552ab0901f3b4
ARDIANSYAH PUTRA - A2RJ <47985349+A2RJ@users.noreply.github.com>
2020-12-20 11:24:20 +0800

    add js file

--- g.php
+++ g.php
@@ -1,9 +1,34 @@
 <?php
+// php g.php -MyourFileName
+require 'vendor/autoload.php';
+$class = new Nette\PhpGenerator\ClassType('Demo');
+use Nette\PhpGenerator\Literal;
+
 $options = getopt("M:");
-$exetension = 'yourFileExetension';
-$file = $options['M'] . $exetension;
-$current = file_get_contents($file);
-$template = "will generate ".$options['M'];
-$current .= $template;
-file_put_contents('yourDirectory'.$file, $current);
+$fileName = $options['M'] . '.php';
+
+// Change for custom result
+$class
+	->setFinal()
+	->setExtends(BaseController::class)
+	->addTrait(App\Models\Jurnal::class)
+	->addComment("Description of class.\nSecond line\n")
+    ->addComment('@property-read Nette\Forms\Form $form')
+    ->addProperty('Jurnal');
+    // construct
+$method = $class->addMethod('__construct')
+    ->setBody('$Jurnal = new Jurnal();');
+$class->addMethod('index')
+->setBody('return $a + $b;')
+->addParameter('id');
+
+
+$current = file_get_contents($fileName);
+
+$current .= '<?php
+' . $class;
+
+// to generate PHP code simply cast to string or use echo:
+// echo $class;
+file_put_contents('yourDirectory'.$fileName, $current);
 ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e9cd4ccf18fabc2112cb8539f95704d6aa6f91cb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e9cd4ccf18fabc2112cb8539f95704d6aa6f91cb',
+    ),
+    'nette/php-generator' => 
+    array (
+      'pretty_version' => 'v3.5.1',
+      'version' => '3.5.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe54415cd22d01bee1307a608058bf131978610a',
+    ),
+    'nette/utils' => 
+    array (
+      'pretty_version' => 'v3.2.0',
+      'version' => '3.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd0427c1811462dbb6c503143eabe5478b26685f7',
+    ),
+  ),
+);
