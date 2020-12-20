560a36366b2ad4722f721c43c854ba937517eb74
ARDIANSYAH PUTRA - A2RJ <47985349+A2RJ@users.noreply.github.com>
2020-12-20 15:48:56 +0800

    generate Controller file

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require('fs-extra');
 const param = process.argv.slice(2);
 const template = "Your Template here";
 
-var coba = require('./models/function');
-coba.area(param)
+// var coba = require('./models/function');
+// coba.area(param)
 
 fs.appendFile(param[0]+param[1], template, function (err) {
     if (err) throw err;
